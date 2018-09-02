5dcd104e866a0454cd96d10fb4296a7c469f282c
a <a@a-PC>
2018-09-02 16:32:23 +0530

    comments removed for testing for pushing GitHub repo

--- springBoot/src/main/java/io/java/springBootStarter/EcommerceApi.java
+++ springBoot/src/main/java/io/java/springBootStarter/EcommerceApi.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 //@SpringBootApplication
+
 @EnableAutoConfiguration
 @ComponentScan(basePackages = {"io.java.controller","io.java.service"}) 
 @Configuration
